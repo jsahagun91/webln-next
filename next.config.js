5436504b3140f881a89bf8a3dbcce71c73a35769
Jose Sahagun <jsahagun91@gmail.com>
2022-11-21 21:24:53 -0800

    fix typescript err

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  ignoreBuildErrors: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  }
+}
